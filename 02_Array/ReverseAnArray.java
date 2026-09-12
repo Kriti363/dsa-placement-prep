6aa1fb2c00df2cb076cd34a075bd775b3836b977
Kriti363 <srivastavakriti171024@gmail.com>
2026-09-12 07:06:27 +0000

    ReverseArray

---
+++ 02_Array/ReverseAnArray.java
@@ -0,0 +1,29 @@
+// Reversing of an Array-:
+
+class ReverseAnArray
+{
+    static void reverse(int[] arr)
+    {
+        int left = 0, right = arr.length - 1;
+        while (left < right)
+        {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+    public static void main (String[] args)
+    {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        reverse(arr);
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
