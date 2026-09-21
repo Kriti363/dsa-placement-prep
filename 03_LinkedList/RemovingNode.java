c2f880621f01d1dd21c6583897ca7f6f4fd10422
Kriti363 <srivastavakriti171024@gmail.com>
2026-09-21 06:43:09 +0000

    RemovingNode

---
+++ 03_LinkedList/RemovingNode.java
@@ -0,0 +1,101 @@
+public class RemovingNode
+{
+    public static void main (String arg[])
+    {
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.print();
+        list.remove(3);
+        list.print();
+    }
+}
+
+class LinkedList
+{
+    class Node
+    {
+        int data;
+        Node next;                             // By default next pointer will be null.
+
+        Node (int data)
+        {
+            this.data = data;              // To initialize the value of data we made a constructor.
+        }
+    }
+
+    Node head;
+
+    void add(int data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+            return;
+        }
+        Node curr = head;
+        while (curr.next != null)
+        {
+            curr = curr.next;
+        }
+        curr.next = newNode;
+    }
+
+    void add(int data, int n)
+    {
+        Node newNode = new Node(data);
+
+        if (n == 1)
+        {
+            newNode.next = head;
+            head = newNode;
+            return;
+        }
+
+        Node curr = head;
+        for (int i = 1; i < n - 1; i++)
+        {
+            curr = curr.next;
+        }
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+    }
+
+    void remove (int n)
+    {
+        if (n == 1)
+        {
+            head = head.next;
+            return;
+        }
+
+        Node curr = head;
+        for (int i = 1; i < n-1; i++)
+            {
+                curr = curr.next;
+            }
+        if (curr != null)
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            {
+                System.out.println("Invalid Position");
+            }
+    }
+
+    void print()
+    {
+        Node curr = head;
+        while (curr != null)
+        {
+            System.out.println(curr.data);
+            curr = curr.next;
+        }
+    }
+}
